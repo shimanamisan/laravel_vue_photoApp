d629b4fea375c320ec61a22cabe40883f1cd041a
しまなみさん <mikan.sup.all@gmail.com>
2020-05-12 13:31:06 +0900

    webpack.mixファイルを編集
    browserSyncを使えるようにしました

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,34 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.js("resources/js/app.js", "public/js")
+    // SCSSのビルド（JSと同じフォルダ構成）
+    .sass("resources/sass/style.scss", "public/css")
+    .options({
+        // laravelにはAutoprefixerが最初から入っていて、必要なプレフィックスを自動的に適用
+        autoprefixer: {
+            options: {
+                browsers: ["last 2 versions"]
+            }
+        }
+    })
+    // versionメソッドで、コンパイルしたファイルのバージョニングを有効にする
+    // ビルドするたびにコンパイルしたファイルの URL にランダムな文字列を付けてブラウザがキャッシュを読まないようにする
+    .version()
+    .sourceMaps();
+
+// ローカルサーバ立ち上げの設定
+mix.browserSync({
+    files: [
+        "resource/views/**/*.blade.php",
+        // 公開フォルダを指定
+        "public/**/*.*"
+    ],
+    proxy: {
+        // laravel側で立ち上げる内部サーバと合わせる
+        target: "http://127.0.0.1:8000"
+    }
+});
